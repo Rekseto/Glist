75d6c99e2fda48e987b46f819b8919c363754413
Rekseto <ReksetoAjxer@gmail.com>
2019-03-20 00:33:37 +0100

    Usage

--- backend/src/routes/statRoute.js
+++ backend/src/routes/statRoute.js
@@ -1,5 +1,6 @@
-module.exports = function(router, { logger }) {
-  router.get("/", (ctx, next) => {
+module.exports = function(router, { logger, statistics }) {
+  router.post("/", (ctx, next) => {
+    statistics.saveStat(JSON.stringify(ctx.request.body));
     ctx.body = {
       success: true
     };
