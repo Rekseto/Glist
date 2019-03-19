44ed68132c8ff639328ad592fc54f66e8c5760e3
Rekseto <ReksetoAjxer@gmail.com>
2019-03-20 00:01:35 +0100

    Statistics draft

--- backend/src/Statistics.js
+++ backend/src/Statistics.js
@@ -1,12 +1,63 @@
 const fs = require("fs");
+const fsPromises = require("fs").promises;
+const fsHelpers = require("./helpers/fsHelpers");
 
 class Statistics {
   constructor(options) {
     this.statsPath = options.statsPath;
   }
+  /**
+   * Saves particular content of metrics to file
+   *
+   * @param   {string} content        Content to save
+   *
+   */
+  async saveStat(content) {
+    const { statsPath } = this;
+    const date = new Date()
+      .toLocaleDateString("en-US")
+      .split("/")
+      .join(".");
 
-  saveStat() {
-    const date = new Date().toLocaleDateString("en-US");
+    const fileName = `${statsPath}/${date}.stats`;
+
+    const text = content + "\r\n";
+    try {
+      await fsPromises.appendFile(fileName, text, "utf8");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+   * Returns metrics for given range.
+   *
+   * @param   {Date}    startingDate       starting range Date
+   * @param   {Date}    endingDate         ending range Date
+   * @return  {Array}
+   */
+  async getStats(startingDate, endingDate) {
+    // @TODO
+    const { statsPath } = this;
+    try {
+      const names = await fsPromises.readdir(statsPath);
+
+      for await (const name of names) {
+        const date = new Date(name.substring(0, name.length - 6));
+        if ((date > startingDate) & (date < endingDate)) {
+          const readStream = fs.createReadStream(`${statsPath}/${name}`, {
+            encoding: "utf8",
+            highWaterMark: 1024
+          });
+
+          for await (const chunk of readStream) {
+            console.log(chunk);
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
