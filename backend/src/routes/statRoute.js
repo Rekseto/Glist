d2485cafbcbea347cd27dd51598446d2f70f31a3
Rekseto <ReksetoAjxer@gmail.com>
2019-03-20 23:30:31 +0100

    Removing useless fs based stats

--- backend/src/routes/statRoute.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(router, { logger, statistics }) {
-  router.post("/", (ctx, next) => {
-    statistics.saveStat(JSON.stringify(ctx.request.body));
-    ctx.body = {
-      success: true
-    };
-  });
-};
