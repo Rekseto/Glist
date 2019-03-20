88dd8e3ff1c386acebb68cfb062fd8c6aaaed7da
Rekseto <ReksetoAjxer@gmail.com>
2019-03-20 23:59:43 +0100

    Fetching metrics in range

--- backend/src/api/routes/metricRoute.js
+++ backend/src/api/routes/metricRoute.js
@@ -2,15 +2,25 @@ const metricsServices = require("../services/metricsServices");
 
 module.exports = function(router, { logger, database }) {
   const metricService = metricsServices({ database, logger });
-  router.get("/", (ctx, next) => {
-    ctx.body = {
-      success: true
-    };
+  router.post("/", async (ctx, next) => {
+    try {
+      const result = await metricService.fetchInRange(ctx.request.body);
+
+      ctx.body = {
+        success: true,
+        data: result
+      };
+    } catch (error) {
+      logger.error(error.message);
+      ctx.body = {
+        success: false
+      };
+    }
   });
 
-  router.post("/", (ctx, next) => {
+  router.put("/", async (ctx, next) => {
     try {
-      metricService.createMetric(ctx.request.body);
+      await metricService.createMetric(ctx.request.body);
       ctx.body = {
         success: true
       };
